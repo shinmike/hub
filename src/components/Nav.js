57dd512c31c05b420de1054bfb5b0684de4bd140
Michael Shin <shinmike90@gmail.com>
2019-05-30 17:34:04 -0700

    changes to review page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,7 +12,7 @@ const Nav = () => {
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Reviews</a>
